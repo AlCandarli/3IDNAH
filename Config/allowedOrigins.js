1a0bfb00a74f78b5598677f04843238cf1b5e273
Ahmed Mushtaq <www.amnesty55@gmail.com>
2025-09-06 20:29:38 +0300

    Update frontend URL in CORS config

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000", "https://frontend-fg610c88t-al-candarlis-projects.vercel.app"];
 
 module.exports = allowedOrigins;
